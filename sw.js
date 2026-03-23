c0d0fd6fece009d0d48cafe10cbe3b1c69614e0a
magnificolv <magnificox@gmail.com>
2026-03-23 23:09:34 +0200

    feat: Add update checking and application logic
    Integrates PWA update checking functionality and the UI elements to manage it. Users can now be notified of new versions and apply them.

--- sw.js
+++ sw.js
@@ -6,6 +6,11 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
